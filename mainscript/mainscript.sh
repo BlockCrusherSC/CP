94b01f4690680d3ac0f42818cdbfdcca3a25bb6e
UrProbablyBetter <andrew.c.patrone@gmail.com>
2024-11-19 15:23:07 -0500

    Update mainscript.sh

--- mainscript/mainscript.sh
+++ mainscript/mainscript.sh
@@ -230,13 +230,21 @@ echo "exec true" >> /etc/init/control-alt-delete.override
 printlog "Ctrl+Alt+Delete reboot disabled."
 
 #rc.local
-#sudo systemctl status rc-local.service
-#cp /etc/rc.local $BACKUPDIR/rc.local
-#chmod 777 $BACKUPDIR/rc.local
-#printlog "rc.local has been backed up."
-#echo > /etc/rc.local
-#echo 'exit 0' >> /etc/rc.local
-#printlog "Any rc.local startup scripts have been removed."
+if systemctl is-active --quiet rc-local.service;
+then
+	echo "rc-local.service is active."
+
+        # Stop and disable the rc-local service
+	cp /etc/rc.local $BACKUPDIR/rc.local
+	chmod 777 $BACKUPDIR/rc.local
+	printlog "rc.local has been backed up."
+        sudo systemctl stop rc-local.service
+        sudo systemctl disable rc-local.service
+        sudo rm -f /etc/rc.local
+        echo "rc.local has been removed and the service is disabled."
+else
+        echo "rc-local.service is not active."
+fi
 
 #Optional Applictions
     #SSH
@@ -410,6 +418,7 @@ then
  	ufw allow https >> $LOG_FILE
 	printlog "HTTP and HTTPS ports opened."
  	cp /etc/apache2/apache2.conf $BACKUPDIR/apache2.conf
+  	chmod 777 $BACKUPDIR/apache2.conf
   	printlog "apache2.conf backed up."
 elif [[ $dnsstatus == "no" || $dnsstatus == "n" ]];
 then
@@ -430,10 +439,10 @@ read mediastatus
 if [[ $mediastatus == "no" || $mediastatus == "n" ]];
 then
 	#audio files
-	find / -type f \( -name "*.midi" -o -name "*.mid" -o -name "*.mp3" -o -name "*.mp2" -o -name "*.mpa" -o -name "*.abs" -o -name "*.mpega" -o -name "*.au" -o -name "*.snd" -o -name "*.wav" -o -name "*.aiff" -o -name "*.aif" -o -name "*.sid" -o -name "*.flac" -o -name "*.ogg" \) -delete 2>> $LOG_FILE
+	find / -type f \( -name "*.midi" -o -name "*.mid" -o -name "*.mp3" -o -name "*.mp2" -o -name "*.mpa" -o -name "*.abs" -o -name "*.mpega" -o -name "*.au" -o -name "*.snd" -o -name "*.aiff" -o -name "*.aif" -o -name "*.sid" -o -name "*.flac" \) -delete 2>> $LOG_FILE
 	printlog "Audio files removed."
 	#video files
-	find / -type f \( -name "*.mpeg" -o -name "*.mpg" -o -name "*.mpe" -o -name "*.dl" -o -name "*.movie" -o -name "*.movi" -o -name "*.mv" -o -name "*.iff" -o -name "*.anim5" -o -name "*.anim3" -o -name "*.anim7" -o -name "*.avi" -o -name "*.vfw" -o -name "*.avx" -o -name "*.fli" -o -name "*.flc" -o -name "*.mov" -o -name "*.qt" -o -name "*.spl" -o -name "*.swf" -o -name "*.dcr" -o -name "*.dxr" -o -name "*.rpm" -o -name "*.rm" -o -name "*.smi" -o -name "*.ra" -o -name "*.ram" -o -name "*.rv" -o -name "*.wmv" -o -name "*.asf" -o -name "*.asx" -o -name "*.wma" -o -name "*.wax" -o -name "*.wmv" -o -name "*.wmx" -o -name "*.3gp" -o -name "*.mov" -o -name "*.mp4" -o -name "*.avi" -o -name "*.swf" -o -name "*.flv" -o -name "*.m4v" \) -delete 2>> $LOG_FILE
+	find / -type f \( -name "*.mpeg" -o -name "*.mpe" -o -name "*.dl" -o -name "*.movie" -o -name "*.movi" -o -name "*.mv" -o -name "*.iff" -o -name "*.anim5" -o -name "*.anim3" -o -name "*.anim7" -o -name "*.avi" -o -name "*.vfw" -o -name "*.avx" -o -name "*.fli" -o -name "*.flc" -o -name "*.mov" -o -name "*.qt" -o -name "*.spl" -o -name "*.swf" -o -name "*.dcr" -o -name "*.dxr" -o -name "*.rpm" -o -name "*.rm" -o -name "*.smi" -o -name "*.ra" -o -name "*.ram" -o -name "*.rv" -o -name "*.wmv" -o -name "*.asf" -o -name "*.asx" -o -name "*.wma" -o -name "*.wax" -o -name "*.wmv" -o -name "*.wmx" -o -name "*.3gp" -o -name "*.mov" -o -name "*.mp4" -o -name "*.avi" -o -name "*.swf" -o -name "*.flv" -o -name "*.m4v" \) -delete 2>> $LOG_FILE
 	printlog "Video files removed."
 	#image files
 	find /home -type f \( -name "*.tiff" -o -name "*.tif" -o -name "*.rs" -o -name "*.im1" -o -name "*.gif" -o -name "*.jpeg" -o -name "*.jpg" -o -name "*.jpe" -o -name "*.png" -o -name "*.rgb" -o -name "*.xwd" -o -name "*.xpm" -o -name "*.ppm" -o -name "*.pbm" -o -name "*.pgm" -o -name "*.pcx" -o -name "*.ico" -o -name "*.svg" -o -name "*.svgz" \) -delete 2>> $LOG_FILE
