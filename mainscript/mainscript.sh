e73136f1cf4cf92387373e1819740d38ac39292c
UrProbablyBetter <andrew.c.patrone@gmail.com>
2025-01-11 15:11:50 -0500

    Update mainscript.sh

--- mainscript/mainscript.sh
+++ mainscript/mainscript.sh
@@ -491,9 +491,10 @@ read clam
 if [[ $clam == "yes" || $clam == "y" ]];
 then
 	apt-get install clamav -y -qq >> $LOG_FILE
+ 	freshclam
 	printlog "clamav installed. Running clamscan (will take a LONG time)..."
 	manualtask "Clamscan infected files:"
-	clamscan --bell --recursive -i >> $MANUAL_FILE
+	clamscan -r --bell -i / >> $MANUAL_FILE
 	printlog "Scan complete."
 	manualtask "Scan complete."
  else
