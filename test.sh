0d6bb4293b9c937eae060b19240e999f21be5db1
UrProbablyBetter <andrew.c.patrone@gmail.com>
2025-01-12 16:55:42 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -12,3 +12,8 @@ else
     mv /etc/sudoers.bak /etc/sudoers
     exit 1
 fi
+
+grep -Pi
+'^\h*auth\h+(?:required|requisite)\h+pam_wheel\.so\h+(?:[^#\n\r]+\h+)?((?!\2)
+(use_uid\b|group=\H+\b))\h+(?:[^#\n\r]+\h+)?((?!\1)(use_uid\b|group=\H+\b))(\
+h+.*)?$' /etc/pam.d/su
