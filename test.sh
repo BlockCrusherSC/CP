cebcb644c2da7d8bacba4ea8e3b7e0cb87d49174
UrProbablyBetter <andrew.c.patrone@gmail.com>
2025-01-13 16:06:34 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -17,3 +17,14 @@ grep -Pi
 '^\h*auth\h+(?:required|requisite)\h+pam_wheel\.so\h+(?:[^#\n\r]+\h+)?((?!\2)
 (use_uid\b|group=\H+\b))\h+(?:[^#\n\r]+\h+)?((?!\1)(use_uid\b|group=\H+\b))(\
 h+.*)?$' /etc/pam.d/su
+
+{
+while IFS= read -r l_user; do
+l_change=$(date -d "$(chage --list $l_user | grep '^Last password
+change' | cut -d: -f2 | grep -v 'never$')" +%s)
+if [[ "$l_change" -gt "$(date +%s)" ]]; then
+echo "User: \"$l_user\" last password change was \"$(chage --list
+$l_user | grep '^Last password change' | cut -d: -f2)\""
+fi
+done < <(awk -F: '$2~/^\$.+\$/{print $1}' /etc/shadow)
+}
