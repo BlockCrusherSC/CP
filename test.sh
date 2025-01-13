a32b230ac9acbbad41a649735c0272cc517c5ba7
UrProbablyBetter <andrew.c.patrone@gmail.com>
2025-01-13 16:07:25 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,22 +1,23 @@
-cp /etc/sudoers /etc/sudoers.bak
+#!/bin/bash
+#cp /etc/sudoers /etc/sudoers.bak
 
 # Use sed to replace the line containing "Defaults"
-sed -i '/^Defaults /c\Defaults use_pty' /etc/sudoers
+#sed -i '/^Defaults /c\Defaults use_pty' /etc/sudoers
 
 # Validate the sudoers file for syntax correctness
-visudo -c
-if [ $? -eq 0 ]; then
-    echo "The sudoers file was updated and is valid."
-else
-    echo "The sudoers file is invalid. Restoring the original."
-    mv /etc/sudoers.bak /etc/sudoers
-    exit 1
-fi
+#visudo -c
+#if [ $? -eq 0 ]; then
+#    echo "The sudoers file was updated and is valid."
+#else
+#    echo "The sudoers file is invalid. Restoring the original."
+#    mv /etc/sudoers.bak /etc/sudoers
+#    exit 1
+#fi
 
-grep -Pi
-'^\h*auth\h+(?:required|requisite)\h+pam_wheel\.so\h+(?:[^#\n\r]+\h+)?((?!\2)
-(use_uid\b|group=\H+\b))\h+(?:[^#\n\r]+\h+)?((?!\1)(use_uid\b|group=\H+\b))(\
-h+.*)?$' /etc/pam.d/su
+#grep -Pi
+#'^\h*auth\h+(?:required|requisite)\h+pam_wheel\.so\h+(?:[^#\n\r]+\h+)?((?!\2)
+#(use_uid\b|group=\H+\b))\h+(?:[^#\n\r]+\h+)?((?!\1)(use_uid\b|group=\H+\b))(\
+#h+.*)?$' /etc/pam.d/su
 
 {
 while IFS= read -r l_user; do
