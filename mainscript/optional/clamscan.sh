a02d31902c7807feb88e70e9306ebca9d17cd70c
UrProbablyBetter <andrew.c.patrone@gmail.com>
2025-01-15 12:54:39 -0500

    Update clamscan.sh

--- mainscript/optional/clamscan.sh
+++ mainscript/optional/clamscan.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+echo "Do you want to clamscan?"
+read clam
+if [[ $clam == "yes" || $clam == "y" ]];
+then
+  apt-get install clamav -y -qq >> $LOG_FILE
+	printlog "clamav installed. Running clamscan (will take a LONG time)..."
+	manualtask "Clamscan infected files:"
+	clamscan -r --bell -i --exclude-dir="^/sys" / >> $MANUAL_FILE
+	printlog "Scan complete."
+	manualtask "Scan complete."
+ else
+ 	printlog "Clamscan not run."
+fi
